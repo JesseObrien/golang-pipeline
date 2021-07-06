3b70e9afde730597b437d211a9e9a4b1ed107ed9
Jamie Cuthill <jc@flyt.io>
2021-07-06 16:52:28 +0100

    Support private repositories by using ssh credentials

--- go1.16/linter/gp-setup.sh
+++ go1.16/linter/gp-setup.sh
@@ -3,6 +3,8 @@ set -exuo pipefail
 
 export GO111MODULE=on
 
+git config --global url."https://${GITHUB_TOKEN}:x-oauth-basic@github.com/".insteadOf "https://github.com"
+
 WORK_SPACE=${PROJECT_PATH:-}
 
 # Set PROJECT_PATH to change your working directory
